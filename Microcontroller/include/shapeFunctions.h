2858abc4b496e542e72fbfd440ca3d58ee3e1384
ronnywestphal <84048902+ronnywestphal@users.noreply.github.com>
2022-08-06 09:48:42 +0200

    Update shapeFunctions.h

--- Microcontroller/include/shapeFunctions.h
+++ Microcontroller/include/shapeFunctions.h
@@ -6,10 +6,12 @@ typedef struct{
     short int ycor;
 }kordinat;
 
-void CAN_receive_message_execute(can_receive_message_struct message,int P_coordinates[],short int *pX,short int *pY);
-void travel(int x0, int y0, int x1, int y1);
 kordinat plot_line (int x0, int y0, int x1, int y1,int x2,int y2);
 kordinat drawcircle(int radie,int x0, int y0, int xst,int yst);
+
+void CAN_receive_message_execute(can_receive_message_struct message,int P_coordinates[],short int *pX,short int *pY);
+
+void travel(int x0, int y0, int x1, int y1);
 void mario();
 void square(int lenght, int midX,int midY);
 void pwm_up(void);
