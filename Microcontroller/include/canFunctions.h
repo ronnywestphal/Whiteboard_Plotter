9d37399241979579d84e696611d5ddc508b149ea
ronnywestphal <84048902+ronnywestphal@users.noreply.github.com>
2022-08-06 09:49:59 +0200

    Update canFunctions.h

--- Microcontroller/include/canFunctions.h
+++ Microcontroller/include/canFunctions.h
@@ -2,7 +2,6 @@
 
 #define CAN_RECEIVE_BUFFER_SIZE 32
 
-
 typedef struct{
     short int xcor;
     short int ycor;
@@ -19,7 +18,6 @@ int can_buffer_push(can_receive_message_struct dat, can_rx_buffer_t *buf);
 can_receive_message_struct can_buffer_pop(can_rx_buffer_t *buf);
 
 void can_buffer_init(can_rx_buffer_t *buf);
-
 void can_networking_init(void);
 
 void CAN_send_message(uint16_t message_id, uint8_t remote_data, uint8_t length, uint8_t *p_data);
