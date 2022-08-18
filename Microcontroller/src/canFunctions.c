ff9d9969465ba4f33cb12d4227cbb27f9bd08fc4
ronnywestphal <84048902+ronnywestphal@users.noreply.github.com>
2022-08-18 12:48:41 +0200

    Update canFunctions.c

--- Microcontroller/src/canFunctions.c
+++ Microcontroller/src/canFunctions.c
@@ -112,7 +112,6 @@ void can_networking_init(void){
     
 }
 
-
 /* Send CAN message routine */
 void CAN_send_message(uint16_t message_id, uint8_t remote_data, uint8_t length, uint8_t *p_data){
     
@@ -143,52 +142,4 @@ void CAN1_RX1_IRQHandler(can_rx_buffer_t *pReceive_buffer){
 
     /* Put message in message buffer and handle in main loop */
     can_buffer_push(receive_message, &pReceive_buffer);
-
-}
-
-void init_PWM(){
-
-    /* These structs are used for configuring the timer */
-    timer_oc_parameter_struct timer_ocinitpara;
-    timer_parameter_struct timer_initpara;
-
-    /* First we need to enable the clock for the timer */
-    rcu_periph_clock_enable(RCU_TIMER4);
-
-    /* Reset the timer to a known state */
-    timer_deinit(TIMER4);
-
-    /* This function sets the struct up with default values */
-    timer_struct_para_init(&timer_initpara);
-
-    /* timer configuration */
-    timer_initpara.prescaler         = 107;                   // Prescaler 1 gives counter clock of 108MHz/2 = 54MHz 
-    timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;  // count alignment edge = 0,1,2,3,0,1,2,3... center align = 0,1,2,3,2,1,0
-    timer_initpara.counterdirection  = TIMER_COUNTER_UP;    // Counter direction
-    timer_initpara.period            = 20000;                // Sets how far to count. 54MHz/4096 = 13,2KHz (max is 65535)
-    timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;    // This is used by deadtime, and digital filtering (not used here though)
-    timer_initpara.repetitioncounter = 0;                   // Runs continiously
-    timer_init(TIMER4, &timer_initpara);                    // Apply settings to timer
-
-
-    /* This function initializes the channel setting struct */
-    timer_channel_output_struct_para_init(&timer_ocinitpara);
-    /* PWM configuration */
-    timer_ocinitpara.outputstate  = TIMER_CCX_ENABLE;                   // Channel enable
-    timer_ocinitpara.outputnstate = TIMER_CCXN_DISABLE;                 // Disable complementary channel
-    timer_ocinitpara.ocpolarity   = TIMER_OC_POLARITY_HIGH;             // Active state is high
-    timer_ocinitpara.ocnpolarity  = TIMER_OCN_POLARITY_HIGH;    
-    timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;            // Idle state is low
-    timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
-    timer_channel_output_config(TIMER4,TIMER_CH_1,&timer_ocinitpara);   // Apply settings to channel
-
-    timer_channel_output_pulse_value_config(TIMER4,TIMER_CH_1,0);                   // Set pulse width
-    timer_channel_output_mode_config(TIMER4,TIMER_CH_1,TIMER_OC_MODE_PWM0);         // Set pwm-mode
-    timer_channel_output_shadow_config(TIMER4,TIMER_CH_1,TIMER_OC_SHADOW_DISABLE);
-
-    /* auto-reload preload enable */
-    timer_auto_reload_shadow_enable(TIMER4);
-
-    /* start the timer */
-    timer_enable(TIMER4);
 }
